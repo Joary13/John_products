a0234606b4dcab83f963461a5d818577da2bf575
Joary13 <jolea13008@gmail.com>
2023-11-10 15:53:43 +0100

    change with window.location.search

--- product.js
+++ product.js
@@ -1,17 +1,19 @@
-const URL = 'https://course-api.com/javascript-store-single-product';
+const URL = 'https://course-api.com/javascript-store-single-product?id=';
 
 const product = document.querySelector('.product');
 
 const getSingleProduct = () => {
-  return window.location.href.slice(34);
+  //console.log(window.location.search);
+  const params = new URLSearchParams(window.location.search);
+  return params.get('id');
 };
 
 const fetchProduct = async () => {
-  product.innerHTML = "<div class='loading'></div>";
+  product.innerHTML = `<h4 class="product-loading>Loading</h4>`;
   const productId = getSingleProduct();
   try {
     console.log(URL + productId);
-    const response = await fetch(URL + productId);
+    const response = await fetch(`${URL}${productId}`);
     if (!response.ok) throw new Error('URL PROBLEM');
     const data = await response.json();
     console.log(data);
@@ -25,11 +27,12 @@ const displayProduct = (obj) => {
   const { id } = obj;
   const { url: image } = obj.fields.image[0];
   const { company, name: title, price, colors, description } = obj.fields;
+  document.title = title.toUpperCase();
   const html = `
    <div class="product-wrapper">
         <img src="${image}" class="img" alt="" />
         <div class="product info">
-          <h3>${name}</h3>
+          <h3>${title}</h3>
           <h5>${company}</h5>
           <span>$${price / 100}</span>
           <div class="colors">
